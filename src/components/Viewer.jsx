27f16a2122a6fa84f0815055b52d7a8346a156bf
Cattamoo <ouob77@kakao,com>
2022-12-17 01:31:50 +0900

    modify image slider

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -4,11 +4,11 @@ import {IoMdOpen} from "react-icons/io";
 
 export default function Viewer({ item }) {
 	return (
-		<div className={`flex flex-col w-full h-1/3 px-4 duration-300`}>
+		<div className={`flex flex-col w-full h-3/7 px-4 duration-300 sm:h-1/3`}>
 			<div className="flex justify-end font-semibold">
 				<a href={item.url || `/${item.title}`} target="_blank" rel="noreferrer">{item.title}<IoMdOpen className="inline ml-1" /></a>
 			</div>
-			{ item.images && <ImageSlider className="self-center" width="w-96" height="h-64" auto={null} items={item.images} /> }
+			{ item.images && <ImageSlider className="self-center" width="w-screen sm:w-[500px]" height="h-64" auto={2500} items={item.images} /> }
 			<div className="text-left">
 				{item.description}
 			</div>
