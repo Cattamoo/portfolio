e516893ad4ed19a6bf704738e4a36789da62f249
Cattamoo <ouob77@kakao,com>
2022-12-16 11:50:09 +0900

    remove skill message

--- src/components/SkillItem.jsx
+++ src/components/SkillItem.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import Progress from "./Progress";
 
-export default function SkillItem({ title, size, color, message }) {
+export default function SkillItem({ title, size, color }) {
 	return (
 		<li className="group cursor-pointer relative flex flex-col items-center justify-center">
 			<span className="font-title">{title}</span>
 			<Progress size={size} color={color} />
-			<div className="scale-0 absolute top-full z-10 w-max max-w-[35vw] break-keep bg-white/90 border-2 p-2 m-1 duration-300 rounded-lg group-hover:scale-100">
-				{message}
-			</div>
 		</li>
 	);
 }
