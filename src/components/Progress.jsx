f446a16f4f221e8a7ac6355d148d98f1364944d1
Cattamoo <ouob77@kakao,com>
2022-12-16 11:57:30 +0900

    change progress font size

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -5,7 +5,7 @@ export default function Progress({ size, color }) {
 		<div className={`relative w-16 h-16 rounded-full overflow-hidden`}>
 			<div className={`w-full h-full ${color} opacity-50`} />
 			<div className={`absolute bottom-0 w-full ${color}`} style={{ height: `${size}%` }} />
-			<div className="absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 text-white">{ `${size}%` }</div>
+			<div className="absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 text-white text-xs">{ `${size}%` }</div>
 		</div>
 	);
 }
