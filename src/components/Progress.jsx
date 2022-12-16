ea192cd2c1cc020f06d6ef0d5e4f9fd0798f3b65
Cattamoo <ouob77@kakao,com>
2022-12-16 15:38:24 +0900

    implementation a wave effect

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -4,7 +4,7 @@ export default function Progress({ size, color }) {
 	return (
 		<div className={`relative w-16 h-16 rounded-full overflow-hidden`}>
 			<div className={`w-full h-full ${color} opacity-50`} />
-			<div className={`absolute bottom-0 w-full ${color}`} style={{ height: `${size}%` }} />
+			<div className={`wave absolute w-full ${color}`} style={{ height: `${size}%`, top: `${100 - size}%`, left: `-1rem` }} />
 			<div className="absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 text-white text-xs">{ `${size}%` }</div>
 		</div>
 	);
