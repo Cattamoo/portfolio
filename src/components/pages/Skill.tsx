008aec95668166a505fb43086b30663be5e38fff
Cattamoo <ouob77@kakao,com>
2023-01-08 15:36:25 +0900

    Skill Popup

--- src/components/pages/Skill.tsx
+++ src/components/pages/Skill.tsx
@@ -1,10 +1,38 @@
 import PageLayout from "../layout/PageLayout";
 import Title from "../ui/Title";
+import Progress from "../ui/Progress";
+
+type SkillItemType = {
+	title: string;
+	size: number;
+	color: string;
+}
 
 export default function Skill() {
+	const skills: SkillItemType[] = [
+		{title: 'HTML', size: 95, color: 'bg-[#e34c26]'},
+		{title: 'CSS', size: 78, color: 'bg-[#264de4]'},
+		{title: 'Javascript', size: 97.6, color: 'bg-[#f7df1e]'},
+		{title: 'Typescript', size: 67.2, color: 'bg-[#3178c6]'},
+		{title: 'React', size: 83, color: 'bg-[#61dafb]'},
+	]
 	return (
 		<PageLayout>
 			<Title title="Skills" />
+			<ul className="grid grid-cols-3 gap-2">
+				{
+					skills.map(({ title, size, color }) => <SkillItem title={title} size={size} color={color} />)
+				}
+			</ul>
 		</PageLayout>
+	);
+}
+
+function SkillItem({ title, size, color }: SkillItemType) {
+	return (
+		<li className="group relative flex flex-col items-center justify-center">
+			<span className="font-title">{title}</span>
+			<Progress size={size} color={color} />
+		</li>
 	);
 }
---
+++ src/components/ui/Progress.tsx
@@ -0,0 +1,14 @@
+type Props = {
+	size: number;
+	color: string;
+}
+
+export default function Progress({ size, color }: Props) {
+	return (
+		<div className={`relative w-16 h-16 rounded-full overflow-hidden`}>
+			<div className={`w-full h-full ${color} opacity-50`} />
+			<div className={`wave absolute w-full ${color}`} style={{ height: `${size}%`, top: `${100 - size}%`, left: `-1rem` }} />
+			<div className="absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 text-white text-xs">{ `${size}%` }</div>
+		</div>
+	);
+}
